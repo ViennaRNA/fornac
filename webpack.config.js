176b1f7ef0817e29d48cf4b6f15743157a888e31
Stefan Hammer <s.hammer@univie.ac.at>
2019-05-17 18:21:54 +0200

    add eslint loader

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: ['babel-loader', 'eslint-loader'],
       },
       {
         test: /\.css$/,
