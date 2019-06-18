b080288b3bce977ddaa2395d6c42bc0d7be76ec5
Stefan Hammer <s.hammer@univie.ac.at>
2019-06-18 09:37:55 +0200

    Also export RNAUtilities adn ColorScheme

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ export {FornaContainer} from './fornac.js';
 export {rnaPlot} from './rnaplot.js';
 export {rnaTreemap} from './rnatreemap.js';
 
-//export {RNAUtilities, ColorScheme} from './rnautils.js';
+export {RNAUtilities, ColorScheme} from './rnautils.js';
 //export {ProteinGraph, RNAGraph, moleculesToJson} from './rnagraph.js';
 //export {simpleXyCoordinates} from './simplernaplot.js';
